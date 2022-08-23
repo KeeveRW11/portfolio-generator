90e3ca29670b4dec921f92be1dfc52f3ab5ae69f
KeeveRW11 <keevewhyte+github@gmail.com>
2022-08-23 12:28:41 -0400

    generate HTML

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
-    console.log(profileDataArgs);
+const fs = require('fs'); 
+const generatePage = require('./src/page-template.js');//importing function so place it at the top 
+
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+/*
+const name = profileDataArgs[0];
+const github = profileDataArgs [0];
 
 
 
-    
+   
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
@@ -17,4 +25,16 @@ profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(printProfileData);
+*/
 
+
+  
+  /*console.log(name, github);
+  console.log(generatePage('name', 'github'));*/
+  fs.writeFile('index.html'/*filename*/ ,generatePage(name, github)/*data being written*/, err => {
+    if (err) throw new Error(err);/*error callback function*/
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+
+  
