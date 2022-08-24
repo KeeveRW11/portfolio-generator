5b03a6ee57d7ddea09dc29438c36961247cf496b
KeeveRW11 <keevewhyte+github@gmail.com>
2022-08-24 12:14:22 -0400

    set inquirer prompt

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
-const fs = require('fs'); 
-const generatePage = require('./src/page-template.js');//importing function so place it at the top 
+const inquirer = require('inquirer');
 
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-/*
-const name = profileDataArgs[0];
-const github = profileDataArgs [0];
-
-
-
-   
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-    }
-
-
-console.log('================');
-
-// Is the same as this...
-profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(printProfileData);
-*/
-
-
-  
-  /*console.log(name, github);
-  console.log(generatePage('name', 'github'));*/
-  fs.writeFile('index.html'/*filename*/ ,generatePage(name, github)/*data being written*/, err => {
-    if (err) throw new Error(err);/*error callback function*/
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
-
+//const fs = require('fs'); 
+//const generatePage = require('./src/page-template.js');//importing function so place it at the top 
+//const pageHTML =generatePage(name, github);
+//fs.writeFile('index.html'/*filename*/ ,pageHTML, err => {
+//  if (err) throw err;/*error callback function*/
+//console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
   
